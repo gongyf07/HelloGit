c2c4ca66e9a60d53c9b4dcdaedea41fb875e3813
gongyf <gongyf07@gmail.com>
2012-02-08 16:18:31 +0800

    change1

--- src/com/thss/HelloGitActivity.java
+++ src/com/thss/HelloGitActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class HelloGitActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	int test;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
